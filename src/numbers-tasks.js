d8f3b1484d2ec43174e5b2570d37cfa86c2ad169
Nastassia Romanova <romanova7410020@gmail.com>
2025-09-14 14:12:41 +0300

    feat: add toNumber task

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -225,8 +225,12 @@ function isPrime(n) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  const num = Number(value);
+  if (Number.isNaN(num)) {
+    return def;
+  }
+  return num;
 }
 
 /**
